120b7e340d822840852d3635c2479b6b59cecbab
Ruhul Amin <ruhulinfo50@gmail.com>
2020-11-25 23:16:50 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import Search from './components/Search';
 import ImageList from './components/ImageList';
 
 class App extends React.Component {
 
-  onSubmit = (term) =>{
-    console.log(term);
+  state = { image:[] }
+
+  onSubmit = async (term) =>{
+   const response = await axios.get('https://api.unsplash.com/search/photos',{
+      params:{query:term},
+      headers:{
+        Authorization: 'Client-ID WVRzZnSC0YPccWLxxgD3zO2l22AfpXSOw3-haanPKMs'
+      }
+    });
+
+    this.setState({image: response.data.results});
   }
   render(){
+    const imgSize = this.state.image.length === 0 ? ' ' : `${this.state.image.length} images found`;
     return (
       <div className="App">
         <Search onSubmit = {this.onSubmit}/>
         <ImageList/>
+        <h6>{imgSize}</h6>
       </div>
     );
   }
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './imageList.css';
+import ImageCard from './ImageCard';
 
 class ImageList extends React.Component{
 
@@ -7,11 +8,7 @@ class ImageList extends React.Component{
         return(
             <div>
                 <div className='image-list'>
-                    <img src="https://images.pexels.com/photos/1083822/pexels-photo-1083822.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
-                    <img src="https://images.pexels.com/photos/931177/pexels-photo-931177.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
-                    <img src="https://images.pexels.com/photos/2106037/pexels-photo-2106037.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
-                    <img src="https://images.pexels.com/photos/1624076/pexels-photo-1624076.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
-                    <img src="https://images.pexels.com/photos/2058498/pexels-photo-2058498.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
+                   <ImageCard/> 
                 </div>
             </div>
         )
